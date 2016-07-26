30e1e3edd499f09b7bf106d3efb3ae3b5ca33639
Jeremy G. Kahn <jeremy@trochee.net>
2016-07-26 14:01:00 -0700

    Bump version: 0.1.2 → 0.2.0

--- islex/__init__.py
+++ islex/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Jeremy G. Kahn'
 __email__ = 'jeremy@trochee.net'
-__version__ = '0.1.2'
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='islex',
-    version='0.1.2',
+    version='0.2.0',
     description="Utility classes to support easy loading and manipulation of dictionary-to-pronunciation.",
     long_description=readme + '\n\n' + history,
     author="Jeremy G. Kahn",
